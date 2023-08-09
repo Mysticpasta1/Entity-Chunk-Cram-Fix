6010be09df6b576d5f8feaf13ec9d5cd8e7bd847
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-08-09 07:28:58 +0200

    add a customizable blacklist + only target (optimization) aggressive entitiees instead of all entities

--- src/main/java/com/mystic/eccf/config/ECCFConfig.java
+++ src/main/java/com/mystic/eccf/config/ECCFConfig.java
@@ -2,16 +2,29 @@ package com.mystic.eccf.config;
 
 import com.cleanroommc.configanytime.ConfigAnytime;
 import net.minecraftforge.common.config.Config;
+
+import java.util.*;
 import java.util.function.Supplier;
 @Config(modid = "eccf")
 public class ECCFConfig {
 
     public static int maxEntitiesPerChunk = 25;
 
+    public static String entityBlacklistIds = "minecraft:ender_dragon,minecraft:wither,minecraft:snow_golem";
+
     // Static initializers go after the properties!
     // This will run automatically when you retrieve any properties from this config class
     static {
-        ConfigAnytime.register(ECCFConfig.class);
+        // Load the blacklist from the configuration
+        entityBlacklistIds = loadEntityBlacklist().toString();
     }
 
+    // Custom method to deserialize the stored string back to a list
+    public static List<String> loadEntityBlacklist() {
+        List<String> blacklist = new ArrayList<>();
+        String storedBlacklist = entityBlacklistIds;
+        String[] ids = storedBlacklist.split(",");
+        blacklist.addAll(Arrays.asList(ids));
+        return blacklist;
+    }
 }
